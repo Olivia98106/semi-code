cfd11ffdc82c11436442d6ef2c345055658af465
Olivia98106 <linwei.olivia@gmail.com>
2024-08-23 19:33:17 +0800

    origin

---
+++ grobid/grobid_processor.py
@@ -0,0 +1,89 @@
+from bs4 import BeautifulSoup
+
+COLORS = {
+    "persName": "rgba(0, 0, 255, 1)",  # Blue
+    "s": "rgba(0, 128, 0, 1)",  # Green
+    "p": "rgba(0, 100, 0, 1)",  # Dark Green
+    "ref": "rgba(255, 255, 0, 1)",  # ??
+    "biblStruct": "rgba(139, 0, 0, 1)",  # Dark Red
+    "head": "rgba(139, 139, 0, 1)",  # Dark Yellow
+    "formula": "rgba(255, 165, 0, 1)",  # Orange
+    "figure": "rgba(165, 42, 42, 1)",  # Brown
+    "title": "rgba(255, 0, 0, 1)",  # Red
+    "affiliation": "rgba(255, 165, 0, 1)"  # red-orengi
+}
+
+
+def get_color(name, param):
+    color = COLORS[name] if name in COLORS else "rgba(128, 128, 128, 1.0)"
+    if param:
+        color = color.replace("1)", "0.4)")
+
+    return color
+
+
+class GrobidProcessor:
+    def __init__(self, grobid_client):
+        self.grobid_client = grobid_client
+
+    def process_structure(self, input_path) -> (dict, int):
+        pdf_file, status, text = self.grobid_client.process_pdf("processFulltextDocument",
+                                                                input_path,
+                                                                consolidate_header=True,
+                                                                consolidate_citations=False,
+                                                                segment_sentences=True,
+                                                                tei_coordinates=True,
+                                                                include_raw_citations=False,
+                                                                include_raw_affiliations=False,
+                                                                generateIDs=True)
+
+        if status != 200:
+            return
+
+        coordinates = self.get_coordinates(text)
+        pages = self.get_pages(text)
+
+        return coordinates, len(pages)
+
+    @staticmethod
+    def box_to_dict(box, color=None, type=None):
+
+        item = {"page": box[0], "x": box[1], "y": box[2], "width": box[3], "height": box[4]}
+        if color is not None:
+            item['color'] = color
+
+        if type:
+            item['type'] = type
+
+        return item
+
+    def get_coordinates(self, text):
+        soup = BeautifulSoup(text, 'xml')
+        all_blocks_with_coordinates = soup.find_all(coords=True)
+
+        # if use_sentences:
+        #     all_blocks_with_coordinates = filter(lambda b: b.name != "p", all_blocks_with_coordinates)
+
+        coordinates = []
+        count = 0
+        for block_id, block in enumerate(all_blocks_with_coordinates):
+            for box in filter(lambda c: len(c) > 0 and c[0] != "", block['coords'].split(";")):
+                coordinates.append(
+                    self.box_to_dict(
+                        box.split(","),
+                        get_color(block.name, count % 2 == 0),
+                        type=block.name
+                    ),
+                )
+            count += 1
+        return coordinates
+
+    def get_pages(self, text):
+        soup = BeautifulSoup(text, 'xml')
+        pages_infos = soup.find_all("surface")
+
+        pages = [{'width': float(page['lrx']) - float(page['ulx']), 'height': float(page['lry']) - float(page['uly'])}
+             for page in pages_infos]
+
+        return pages
+
---
+++ prompts/Summary.md
@@ -0,0 +1,7 @@
+Please provide a summary of the research article focusing on the following aspects, using original phrases about time and unit of analysis from article if possible:
+- Research Method: Describe the overall research method employed in the study, also the data collection procedure and duration, time intervals
+- Time relevant details: state the data collection procedure and duration, time intervals of data collection between times. Usually research variables are collected each time.
+- Sampling Method and Entity Type: Explain the sampling method used and specify the type of entities (e.g., individuals, organizations) involved. Here, entity refers to an unit of analysis, or termed as analysis level, granuality or resolution. 
+- Statistical Model: Outline the statistical model applied for analysis. DO NOT USE conceptual model name here.
+- Unit of Analysis: Identify the unit of analysis used in the statistical model.
+- Number of entities or Sample Size: the table and results parts ususally reveal the number of analysis unit.Analysis model details in figure and table are good references.
