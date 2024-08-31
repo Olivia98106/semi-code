f4bf13e5489cb06641993cba3441ee446cbe707f
Charles Liu <charles.liu03@sap.com>
2024-08-31 14:26:18 +0800

    refactor

--- grobid_service.py
+++ grobid_service.py
@@ -1,5 +1,6 @@
 from grobid_client.grobid_client import GrobidClient
 
+# docker run --rm --init --ulimit core=0 -p 8070:8070 lfoppiano/grobid:0.8.0
 if __name__ == "__main__":
     client = GrobidClient(config_path="./grobid_config.json")
     client.process("processFulltextDocument", "./resources/test_pdf/", output="./resources/test_out/", consolidate_citations=True, tei_coordinates=True, force=True)
